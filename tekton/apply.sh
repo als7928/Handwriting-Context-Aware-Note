3c7c806c0b311c75dccf524f75a81fa2fb11e726
als7928 <als7928@daum.net>
2026-04-01 13:17:27 +0900

    fix error

--- tekton/apply.sh
+++ tekton/apply.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+kubectl apply -f 00-task-git-clone.yaml
 kubectl apply -f 01-task-harbor-login.yaml
 kubectl apply -f 02-task-kaniko-build-push.yaml
 kubectl apply -f 03-pipeline.yaml
